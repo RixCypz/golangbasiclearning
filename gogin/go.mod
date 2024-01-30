31781865155bdddead7a9bc805b15554747ea3b2
Chaiyapruek Worasiha <Chaiyapruek@sirisoft.co.th>
2024-01-30 15:07:32 +0700

    [Implement] Build Get Add and Delete without database

--- gogin/go.mod
+++ gogin/go.mod
@@ -16,17 +16,23 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/liudng/dogo v0.0.0-20161102105440-7887b849de84 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/zhgo/config v0.0.0-20150924145423-9caf41330bd9 // indirect
+	github.com/zhgo/console v0.0.0-20150817103316-22d59bcbd28e // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- gogin/main.go
+++ gogin/main.go
@@ -1,26 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github/rixcypz/gin/model"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	fmt.Println("Hello world")
-	r := gin.New()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
-		})
-	})
+var students = []model.Student{
+	{Id: 1, Firstname: "Harry", Lastname: "Potter", Age: 20},
+	{Id: 2, Firstname: "Tomas", Lastname: "Sheldon", Age: 18},
+	{Id: 3, Firstname: "Virgil", Lastname: "Van Lor", Age: 19},
+}
 
-	// Get the port from the PORT environment variable, default to 8080 if not set
+func main() {
+	r := gin.New()
+
+	r.GET("/student", listStudentsHandler)
+	r.POST("/student", createStudentsHandler)
+	r.DELETE("/student/:id", deleteStudentHandler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "9090"
 	}
 	r.Run(":" + port)
+}
+
+func listStudentsHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, students)
+}
+
+func createStudentsHandler(c *gin.Context) {
+	var student model.Student
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	students = append(students, student)
+	c.JSON(http.StatusCreated, student)
+}
+
+func deleteStudentHandler(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+	for i, a := range students {
+		if a.Id == id {
+			students = append(students[:i], students[i+1:]...)
+			break
+		}
+	}
+	c.Status(http.StatusNoContent)
 }
