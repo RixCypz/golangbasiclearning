a94e54fb07e79b462c3f8d4912814d86234c674c
Rixcypz <rixcypz@gmail.com>
2025-06-30 14:43:18 +0700

    [exercise] - palindrome.

--- fundamental/main.go
+++ fundamental/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!!")
+	var name string = "Go lang"
+	age := 10
+	printFullName(age, name)
+	fmt.Println()
+	if isPalindrome("bab") {
+		fmt.Printf("Is palindrome")
+	} else {
+		fmt.Printf("Not palindrome")
+	}
+}
+
+func printFullName(age int, name string) {
+	fmt.Printf("Name is %s and %d years old.", name, age)
+}
+
+func isPalindrome(text string) bool {
+	cleanText := strings.ToLower(strings.ReplaceAll(text, " ", ""))
+	length := len(cleanText)
+	for i := 0; i < length/2; i++ {
+		if cleanText[i] != cleanText[length-1-i] {
+			return false
+		}
+	}
+	return true
 }
